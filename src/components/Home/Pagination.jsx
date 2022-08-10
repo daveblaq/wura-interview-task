3fbaca27ea7e6f80c0a8a8d3f6ed6ee71422eb9e
daveblaq <davidoladele80@gmail.com>
2022-08-10 18:21:12 +0100

    succefully cleaned codebase

--- src/components/Home/Pagination.jsx
+++ src/components/Home/Pagination.jsx
@@ -1,21 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Pagination = () => {
-  // useEffect(() => {
-  //   if (slice.length < 1 && page !== 1) {
-  //     setPage(page - 1);
-  //   }
-  // }, [slice, page, setPage]);
+ 
   return (
     <div className="my-8 flex items-end justify-end pr-3">
-      {/* {range.map((el, index) => ( */}
       <div className={`px-5 py-2 bg-blue-400`}>
         <h2 className="text-sm text-wh"> Prev </h2>
       </div>
       <div className={`px-5 py-2 bg-gray-100`}>
         <h2 className="text-sm text-black"> Next </h2>
       </div>
-      {/* ))} */}
+      
     </div>
   );
 };
